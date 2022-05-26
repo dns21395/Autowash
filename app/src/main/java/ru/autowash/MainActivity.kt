0ce2b65d72a19bb11de82d48f1013b4f38b80fb3
dns21395 <dns21395@gmail.com>
2022-05-26 20:40:11 +0900

    открытие второго экрана

--- app/src/main/java/ru/autowash/MainActivity.kt
+++ app/src/main/java/ru/autowash/MainActivity.kt
@@ -1,11 +1,27 @@
 package ru.autowash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.autowash.databinding.ActivityRegister1Binding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_1)
+
+        val binding = ActivityRegister1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnSignIn.setOnClickListener {
+                startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
+                finish()
+            }
+            btnRegister.setOnClickListener {
+                startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
+                finish()
+            }
+        }
     }
 }
